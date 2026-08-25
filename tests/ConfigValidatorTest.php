6e40c5c12c2b7f141cb25901d37080f81c98b1ac
Kos Huang <koshuang@gmail.com>
2026-08-25 19:28:07 +0800

    test: cover required config validation

---
+++ tests/ConfigValidatorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Koshuang\LaravelConfigGuard\Tests;
+
+use Koshuang\LaravelConfigGuard\Support\ConfigValidator;
+
+class ConfigValidatorTest extends TestCase
+{
+    public function test_it_reports_missing_resolved_config_values(): void
+    {
+        config()->set('payment.stripe.secret', 'secret');
+        config()->set('payment.stripe.webhook_secret', null);
+
+        $missing = (new ConfigValidator())->missing([
+            'payment.stripe.secret',
+            'payment.stripe.webhook_secret',
+        ]);
+
+        $this->assertSame(['payment.stripe.webhook_secret'], $missing);
+    }
+}
